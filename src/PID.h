a6a28a2994f8b5ac44aafd6744f1e90f25383ad1
Silas Ulrich <ulrich@comnovo.de>
2021-08-17 10:22:32 +0200

    Feature: Pid controller implemented, Added PID training, Added Readme

--- src/PID.h
+++ src/PID.h
@@ -4,52 +4,68 @@
 #include <vector>
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
-  PID();
+public:
+    /**
+     * Constructor
+     */
+    PID();
 
-  /**
-   * Destructor.
-   */
-  virtual ~PID();
+    /**
+     * Destructor.
+     */
+    virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_);
+    /**
+     * Initialize PID.
+     * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+     */
+    void Init(double Kp_, double Ki_, double Kd_, double Dp_, double Dd_, double Di_,  double train_thr);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
-  void UpdateError(double cte);
+    /**
+     * Update the PID error variables given cross track error.
+     * @param cte The current cross track error
+     */
+    void UpdateError(double cte);
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
-  double TotalError();
+    /**
+     * Calculate the total PID error.
+     * @output The total PID error
+     */
+    double TotalError() const;
 
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
-  double prev_cte;
+    bool TrainParameters(double cte);
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    void PrintParameters() const;
 
-  std::vector<double> int_cte;
+private:
+    /**
+     * PID Errors
+     */
+    double p_error;
+    double i_error;
+    double d_error;
+    double prev_cte;
+
+    /**
+     * Throttle train parameter
+     */
+    double best_err;
+    double thr;
+    int parameter_ind;
+    int state_ind;
+
+
+    /**
+     * PID Coefficients
+     */
+    double Kp;
+    double Ki;
+    double Kd;
+
+    std::vector<double> int_cte;
+    double p[3];
+    double d[3];
+
 };
 
 #endif  // PID_H
--- src/main.cpp
+++ src/main.cpp
@@ -39,13 +39,26 @@ int main() {
     /**
      * TODO: Initialize the pid variable.
      */
-    double multiply = 0.255;
-    pid.Init(0.225, 0.004,4);
-    pidSpeed.Init(0.225, 0.004,4);
-    double static const ROAD_OFFSET = 0.76;
+    // PID: Kp: 0.5 Kd: 6.1 Ki: 5.1e-08 #1
+    // PID Speed : Kp: 3 Kd: 3.5 Ki: 5.1e-08 #1
+    pid.Init(0.5, 0.000000051, 6.1, 0.02, 0.01, 0.00000001, 0.2);
+    pidSpeed.Init(3, 0.0000000051, 3.5, 1, 1, 0.000000001, 0.001);
 
-    h.onMessage([&pid, &pidSpeed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                                  uWS::OpCode opCode) {
+    static double const ROAD_OFFSET = 0.76;
+    static double const MAX_SPEED = 0.3;
+    static double const MIN_SPEED = 0.1;
+
+    int loop_counter = 1;
+    double mean_err_cte = 0;
+    double mean_err_speed = 0;
+    double mean_error = 0;
+
+    static bool TRAIN_PARAMETER_SPEED_PID = false;
+    static bool TRAIN_PARAMETER_STEERING_PID = false;
+
+    h.onMessage([&pid, &pidSpeed, &loop_counter, &mean_err_speed, &mean_error, &mean_err_cte](
+            uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+            uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
@@ -76,8 +89,48 @@ int main() {
                     pid.UpdateError(cte);
                     pidSpeed.UpdateError(cte);
 
+
+                    /**
+                     * Train PID controller (only one at a time)
+                     * Retrain triggered when reconnect to the simulator
+                     */
+                    if (TRAIN_PARAMETER_SPEED_PID) {
+                        if (loop_counter == -1) {
+                            bool result = pidSpeed.TrainParameters(mean_err_cte);
+                            if (result) {
+                                std::cout << "========= Parameter training complete =========" << std::endl;
+                            }
+
+                            // Reset parameters
+                            mean_error = 0;
+                            mean_err_cte = 0;
+                            mean_err_speed = 0;
+                        }
+                    } else if (TRAIN_PARAMETER_STEERING_PID) {
+                        if (loop_counter == -1) {
+                            bool result = pid.TrainParameters(mean_err_cte);
+                            if (result) {
+                                std::cout << "========= Parameter training complete =========" << std::endl;
+                            }
+                            // Reset parameters
+                            mean_error = 0;
+                            mean_err_cte = 0;
+                            mean_err_speed = 0;
+                        }
+                    }
+
+                    if (loop_counter >= 0) {
+                        // Add error to the power of 2 (negative values in respective)
+                        mean_err_cte += pow(cte, 2);
+                    }
+                    // Increment loop counter
+                    loop_counter = (loop_counter + 1);
+
+
                     steer_value = pid.TotalError();
                     double speed_value = 1 - pidSpeed.TotalError();
+
+                    mean_err_speed += speed_value;
 
                     if (steer_value > 1) {
                         steer_value = 1;
@@ -85,22 +138,21 @@ int main() {
                         steer_value = -1;
                     }
 
-                    if (speed_value > 0.3) {
-                        speed_value = 0.3;
-                    } else if (speed_value < 0.1) {
-                        speed_value = 0.1;
+                    if (speed_value > MAX_SPEED) {
+                        speed_value = MAX_SPEED;
+                    } else if (speed_value < MIN_SPEED) {
+                        speed_value = MIN_SPEED;
                     }
 
-
                     // DEBUG
-                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value << " Speed: " << speed_value
-                              << std::endl;
+//                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value << " Speed: " << speed_value
+//                              << std::endl;
 
                     json msgJson;
                     msgJson["steering_angle"] = steer_value;
                     msgJson["throttle"] = speed_value;
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-                    std::cout << msg << std::endl;
+//                    std::cout << msg << std::endl;
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
                 }  // end "telemetry" if
             } else {
@@ -111,9 +163,26 @@ int main() {
         }  // end websocket message if
     }); // end h.onMessage
 
-    h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-        std::cout << "Connected!!!" << std::endl;
-    });
+    h.onConnection(
+            [&h, &loop_counter, &mean_error, &mean_err_cte, &mean_err_speed, &pidSpeed, &pid](
+                    uWS::WebSocket<uWS::SERVER> ws,
+                    uWS::HttpRequest req) {
+                std::cout << "Connected!!!" << std::endl;
+                pidSpeed.PrintParameters();
+                pid.PrintParameters();
+
+                /**
+                 * Start training loop
+                 */
+                mean_err_speed /= loop_counter;
+                mean_err_cte /= pow(loop_counter, 2);
+
+                mean_error = 0.5 * mean_err_cte + 0.5 * mean_err_speed;
+                std::cout << "[Speed PID] Car moved for " << loop_counter << " with a mean error of " << mean_err_cte
+                          << std::endl;
+
+                loop_counter = -1;
+            });
 
     h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                            char *message, size_t length) {
